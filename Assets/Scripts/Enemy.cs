f3f5a33b6314e1f10d177e01e19f62d8968b0de1
MLanaM <cvetakuleshova1999@gmail.com>
2026-07-30 22:28:13 +0300

    update_1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Renderer))]
 
@@ -6,8 +7,31 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.5f;
 
+    private Coroutine _coroutine;
+
     private void Update()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        _coroutine = StartCoroutine(DestroyEnemy());
+    }
+
+    private void OnDisable()
+    {
+        if (_coroutine == null)
+        {
+            return;
+        }
+
+        StopCoroutine(_coroutine);
+    }
+
+    private IEnumerator DestroyEnemy()
+    {
+        yield return new WaitForSeconds(20f);
+        Destroy(gameObject);
     }
 }
